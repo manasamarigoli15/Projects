62ead0b04023209f6381e7d50b108a52557cd837
manasamarigoli15 <manasamarigoli15@gmail.com>
2025-05-16 17:23:23 +0200

    Added

---
+++ Book_My_Show_Java_Project/BookMyShow-EmailService/src/main/java/com/hm/Capstoneproject/gmail/model/Booking.java
@@ -0,0 +1,28 @@
+package com.hm.Capstoneproject.gmail.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+
+@Data
+@Component
+public class Booking {
+	
+	
+	private int id;
+	private int showId;
+	private LocalDateTime showtime;
+	private String screenName;
+	private String multiplexName;
+	private String cityName;
+	private String multiplexAddress;
+	private String movieName;
+	private String username;
+	private String userEmail;
+	private List<Integer> bookedSeatNos;
+
+}
